dc8be685295295d56ad5aff58442e7eff89a49b0
lo0lode <arthassas@mail.ru>
2022-12-06 13:08:12 +0300

    game

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import prime from '../src/games/brain-prime.js';
+import GamePrime from '../src/games/brain-prime.js';
 
-prime();
+GamePrime();
--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,30 +1,21 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
-const prime = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  let i = 0;
-  let result = '';
-
-  const simple = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
-  while (i < 3) {
-    const random = Math.floor(Math.random() * 50);
-    console.log(`Question: ${random}`);
-    const answer = readlineSync.question('Your answer: ');
-    result = simple.includes(random) ? 'yes' : 'no';
-    console.log(result);
-    if (answer !== result) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'. \n Let's try again, ${name}!`);
-      return;
-    }
-    i += 1;
-    console.log('Correct!');
+import Game from '../index.js';
+import randomNumber from '../randomNumber.js';
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const Prime = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i < num / 2; i += 1) {
+    if (num % i === 0) return false;
   }
-  if (i === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
+  return true;
 };
-export default prime;
+const questionandAnswer=()=>{
+  const question= randomNumber()
+  const answer = Prime(question) ? 'yes' : 'no';
+  return [question,answer]
+}
+const GamePrime = () => {
+  Game(rules, questionandAnswer);
+};
+export default GamePrime
